7d7f12b8d95da37d2363b1b8bf6343b6bbd35d2f
Paul Wilson <paulanthonywilson@gmail.com>
2021-01-24 15:06:23 +0000

    package updates

--- apps/shopping_web/mix.exs
+++ apps/shopping_web/mix.exs
@@ -39,7 +39,7 @@ defmodule ShoppingWeb.MixProject do
     [
       {:phoenix, "~> 1.5.6"},
       {:phoenix_ecto, "~> 4.0"},
-      {:phoenix_live_view, "~> 0.14.6"},
+      {:phoenix_live_view, "~> 0.15.3"},
       {:floki, ">= 0.27.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
